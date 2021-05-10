3c2607a7d3dd59b22daec2eda53cd4779b49bd27
Ruan De Villiers <Ruan.DeVilliers@nintex.com>
2021-05-11 00:18:18 +0200

    Added tops

--- Chrome_Covid_Extension/background.js
+++ Chrome_Covid_Extension/background.js
@@ -1,8 +1,9 @@
 var host = "covid-193.p.rapidapi.com";
 var key = "";
+
 chrome.runtime.onInstalled.addListener(function () {
   var alarmCreateInfo = {
-    periodInMinutes: 0.1,
+    periodInMinutes:0.1,
   };
 
   chrome.alarms.create("CovidUpdate", alarmCreateInfo);
@@ -12,6 +13,7 @@ chrome.runtime.onInstalled.addListener(function () {
       console.log("Host or Key is empty!!!");
       return;
     }
+    var notAllowed = ['Africa','All',"Asia","Europe"];
 
     const myHeaders = new Headers({
       "x-rapidapi-key": key.apiKey,
@@ -25,19 +27,73 @@ chrome.runtime.onInstalled.addListener(function () {
       mode: "cors",
       cache: "default",
     });
+    const worstNewCases = async (data) => {
+      var a = data.reduce(isWorse, { country: "", newCases: 0 });
+      return a;
+    };
+    const worstNewDeaths = async (data) => {
+      var a = data.reduce(isWorseDeaths, { country: "", newDeaths: 0 });
+      return a;
+    };
+
+    function isWorseDeaths(country, countryData) {
+      if (notAllowed.indexOf(countryData.country)<0) {
+        if (parseInt(country.newDeaths)< parseInt(countryData.deaths.new)) {
+          var a = {
+            country: countryData.country,
+            newCases: countryData.cases.new,
+            newDeaths : countryData.deaths.new,
+            time :new Date(countryData.time).toLocaleString()
+          };
+          return a;
+        }
+      }
+      return country;
+    }
+
+    function isWorse(country, countryData) {
+      if (notAllowed.indexOf(countryData.country)<0) {
+        if (parseInt(country.newCases)< parseInt(countryData.cases.new)) {
+          var a = {
+            country: countryData.country,
+            newCases: countryData.cases.new,
+            newDeaths : countryData.deaths.new,
+            time :new Date(countryData.time).toLocaleString()
+          };
+          return a;
+        }
+      }
+      return country;
+    }
 
     fetch(myRequest)
       .then((response) => response.json())
       .then((myBlob) => {
         if (myBlob === undefined) return;
-        var lastRefreshed = (new Date()).toISOString();
+        var lastRefreshed = new Date().toISOString();
         var stats = myBlob.response;
         stats = stats.sort((a, b) => {
           return a.country.localeCompare(b.country);
         });
+        worstNewCases(stats).then((result) => {
+          chrome.storage.local.set(
+            { worstNewCases: result },
+            function () {
+              console.log("WORST New cases : " + JSON.stringify(result));
+            }
+          );
+        });
+        worstNewDeaths(stats).then((result) => {
+          chrome.storage.local.set(
+            { worstDeaths: result },
+            function () {
+              console.log("WORST Deaths : " + JSON.stringify(result));
+            }
+          );
+        });
 
         chrome.storage.local.set(
-          { covidData: stats,lastRefreshed  },
+          { covidData: stats, lastRefreshed },
           function () {
             console.log(stats);
           }
--- Chrome_Covid_Extension/popup.js
+++ Chrome_Covid_Extension/popup.js
@@ -1,32 +1,84 @@
-chrome.storage.local.get("selectedCountry", function (country) {
-  chrome.storage.local.get("covidData", function (covidData) {
-    chrome.storage.local.get("lastRefreshed", function (lr) {
-      let countryName = document.getElementsByName("countryName")[0];
-      let countryflag = document.getElementById("flagImage");
-      countryflag.src =
-        "https://cdn.countryflags.com/thumbs/" +
-        country.selectedCountry.toLowerCase().replace(" ", "-") +
-        "/flag-square-250.png";
-      if (covidData === undefined || covidData.covidData === undefined) {
-        countryName.textContent = "NO DATA";
-      } else {
-        let newCases = document.getElementsByName("newCases")[0];
-        let newDeaths = document.getElementsByName("newDeaths")[0];
-        let lastUpdated = document.getElementsByName("lastUpdated")[0];
-        let lastRefreshed = document.getElementsByName("lastRefreshed")[0];
-        myData = covidData.covidData.filter((c) => {
-          return (
-            c.country.toLowerCase() ===
-            country.selectedCountry.toLowerCase().replace(" ", "-")
-          );
-        })[0];
-        countryName.textContent = myData.country;
-        newCases.textContent = myData.cases.new;
-        newDeaths.textContent = myData.deaths.new;
-        lastUpdated.textContent = new Date(myData.time).toLocaleString();
-        var las = new Date(lr.lastRefreshed).toISOString();
-        lastRefreshed.textContent = new Date(las).toLocaleString().replace('T', ' ').replace(/\.\d+Z/, "");
-      }
+document
+  .getElementById("worstNewCases")
+  .addEventListener("click", showWorstNewCases);
+document
+  .getElementById("worstNewDeaths")
+  .addEventListener("click", showWorstNewDeaths);
+document.getElementById("Selected").addEventListener("click", showSelected);
+
+function showWorstNewCases() {
+  chrome.storage.local.get("worstNewCases", function (country) {
+    let newCases = document.getElementsByName("newCases")[0];
+    let newDeaths = document.getElementsByName("newDeaths")[0];
+    let lastUpdated = document.getElementsByName("lastUpdated")[0];
+    let countryflag = document.getElementById("flagImage");
+
+    myData = country.worstNewCases;
+    countryName.textContent = myData.country;
+    newCases.textContent = myData.newCases;
+    newDeaths.textContent = myData.newDeaths;
+    lastUpdated.textContent = new Date(myData.time).toLocaleString();
+    countryflag.src =
+      "https://cdn.countryflags.com/thumbs/" +
+      myData.country.toLowerCase().replace(" ", "-") +
+      "/flag-square-250.png";
+  });
+}
+
+function showWorstNewDeaths() {
+  chrome.storage.local.get("worstDeaths", function (country) {
+    let newCases = document.getElementsByName("newCases")[0];
+    let newDeaths = document.getElementsByName("newDeaths")[0];
+    let lastUpdated = document.getElementsByName("lastUpdated")[0];
+    let countryflag = document.getElementById("flagImage");
+
+    myData = country.worstDeaths;
+    countryName.textContent = myData.country;
+    newCases.textContent = myData.newCases;
+    newDeaths.textContent = myData.newDeaths;
+    lastUpdated.textContent = new Date(myData.time).toLocaleString();
+    countryflag.src =
+      "https://cdn.countryflags.com/thumbs/" +
+      myData.country.toLowerCase().replace(" ", "-") +
+      "/flag-square-250.png";
+  });
+}
+
+function showSelected() {
+  chrome.storage.local.get("selectedCountry", function (country) {
+    chrome.storage.local.get("covidData", function (covidData) {
+      chrome.storage.local.get("lastRefreshed", function (lr) {
+        let countryName = document.getElementsByName("countryName")[0];
+        let countryflag = document.getElementById("flagImage");
+        countryflag.src =
+          "https://cdn.countryflags.com/thumbs/" +
+          country.selectedCountry.toLowerCase().replace(" ", "-") +
+          "/flag-square-250.png";
+        if (covidData === undefined || covidData.covidData === undefined) {
+          countryName.textContent = "NO DATA";
+        } else {
+          let newCases = document.getElementsByName("newCases")[0];
+          let newDeaths = document.getElementsByName("newDeaths")[0];
+          let lastUpdated = document.getElementsByName("lastUpdated")[0];
+          let lastRefreshed = document.getElementsByName("lastRefreshed")[0];
+          myData = covidData.covidData.filter((c) => {
+            return (
+              c.country.toLowerCase() ===
+              country.selectedCountry.toLowerCase().replace(" ", "-")
+            );
+          })[0];
+          countryName.textContent = myData.country;
+          newCases.textContent = myData.cases.new;
+          newDeaths.textContent = myData.deaths.new;
+          lastUpdated.textContent = new Date(myData.time).toLocaleString();
+          var las = new Date(lr.lastRefreshed).toISOString();
+          lastRefreshed.textContent = new Date(las)
+            .toLocaleString()
+            .replace("T", " ")
+            .replace(/\.\d+Z/, "");
+        }
+      });
     });
   });
-});
+}
+showSelected();
